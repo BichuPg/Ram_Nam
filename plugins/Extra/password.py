263aa6394da46368179a09e68f3be43ca9627e78
BichuPg <115611237+BichuPg@users.noreply.github.com>
2024-06-24 13:03:10 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -19,5 +19,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Venom Stone Network', url='https://t.me/VenomStoneNetwork')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('⚔️ Technical Bots ⚔️', url='https://t.me/Technical_Bots')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
